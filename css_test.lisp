47fb270bb21d3efc522aeebde93b80a6e3372db6
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-01-19 09:24:18 +0200

    improve css generation

--- css_test.lisp
+++ css_test.lisp
@@ -1,2 +1,2 @@
-(rule (body)
-    margin ((px 15)))
+(rule (c html body header (> h1 h2 h3))
+    margin ((px 15) (px 0) (px 15) (px 0)))
--- lang/css.scm
+++ lang/css.scm
@@ -12,47 +12,56 @@
 			(css//declarations->alist (cddr x)))))
 
 (define (css/rule x)
-	(let ((rule (car x)) (declarations (css//declarations->alist (cdr x))))
 	(string-append
-		(string-join (map (partial serialise "css") rule) " ")
+		(serialise "css" (car x))
 		" {\n"
-		(string-join (map css/declaration declarations) ";\n")
-		"\n}")))
+		(css//declarations (css//declarations->alist (cdr x)))
+		"\n}"))
 
-(define (css/declaration x)
-	(let ((property (car x)) (value (cadr x)))
+(define (css//declarations x)
+	(string-join (map css//declaration x) "\n"))
+
+(define (css//declaration x)
+	(let ((property (car x)) (value (cdr x)))
 	(string-append
 		(serialise "css" property)
 		": "
-		(map (partial serialise "css") value))))
+		(string-join (map (partial serialise "css") value) " ")
+		";")))
 
-(define (css//unit unit x) (string-append (serialise "css" x) unit))
+(define (css//unit u x) (string-append (serialise "css" x) u))
 
 (define (css//prefix p x) (string-append p (serialise "css" x)))
 
 (define (css//function f x) (string-append f "(" (map (partial serialise "css") x) ")"))
 
-(define (css/px x) (css//unit (car x) "px"))
+(define (css/px x) (css//unit "px" (car x)))
 
-(define (css/em x) (css//unit (car x) "em"))
+(define (css/em x) (css//unit "em" (car x)))
 
-(define (css/rem x) (css//unit (car x) "rem"))
+(define (css/rem x) (css//unit "rem" (car x)))
 
-(define (css/vh x) (css//unit (car x) "vh"))
+(define (css/vh x) (css//unit "vh" (car x)))
 
-(define (css/vw x) (css//unit (car x) "vw"))
+(define (css/vw x) (css//unit "vw" (car x)))
 
-(define (css/% x) (css//unit (car x) "%"))
-
-(define (css/> x) (css//prefix "> " (car x)))
-
-(define (css/~ x) (css//prefix "~ " (car x)))
-
-(define (css/+ x) (css//prefix "+ " (car x)))
-
-(define (css/hex x) (css//prefix "#" (car x)))
+(define (css/% x) (css//unit "%" (car x)))
 
 (define (css/important x) (css//unit " !important" (car x)))
+
+(define (css/> x)
+	(string-join
+		(map (lambda (x) (string-append "> " (serialise "css" x))) x)))
+
+(define (css/~ x)
+	(string-join
+		(map (lambda (x) (string-append "~ " (serialise "css" x))) x)))
+
+(define (css/+ x)
+	(string-join
+		(map (lambda (x) (string-append "+ " (serialise "css" x))) x)))
+
+(define (css/hex x) (css//prefix "#" (car x)))
 
 (define (css/rgba x) (css//function "rgba" x))
 
@@ -64,7 +73,11 @@
 
 (define (css/: x) (string-append (serialise "css" (cadr x)) ":" (car x)))
 
-(define (css/: x) (string-append (serialise "css" (cadr x)) "::" (car x)))
+(define (css/:: x) (string-append (serialise "css" (cadr x)) "::" (car x)))
+
+(define (css/c x) (string-join (map (partial serialise "css") x) " "))
+
+(define (css/or x) (string-join (map (partial serialise "css") x) ", "))
 
 (define (css/= x)
 	(string-append
